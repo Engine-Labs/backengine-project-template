3a267d1f37ae32687c8cd5f529b8380d6d8636ca
Charlie Cooper <charliecooper98@gmail.com>
2023-08-26 16:27:14 +0100

    handle join table code generation

--- app/project/Hook/RunHook.tsx
+++ app/project/Hook/RunHook.tsx
@@ -1,15 +1,31 @@
+"use client";
+
 import { useState } from "react";
 import type { Metadata } from "../Hooks";
 
+const parseDir = (entityType: "TABLE" | "VIEW") => {
+  if (entityType === "TABLE") {
+    return "tables";
+  }
+
+  return "views";
+};
+
 export default function RunHook({ hookMetadata }: { hookMetadata: Metadata }) {
   const [isError, setIsError] = useState(false);
+
+  if (hookMetadata.entityType === "JOIN_TABLE") {
+    // TODO: handle other types
+    return null;
+  }
 
   let Component = () => null;
   if (!isError) {
     try {
       const sanitisedName = hookMetadata.name.replace("use", "");
-      Component =
-        require(`@/__backengine__/components/${sanitisedName}`).default;
+      Component = require(`@/__backengine__/components/${parseDir(
+        hookMetadata.entityType
+      )}/${sanitisedName}`).default;
     } catch {
       setIsError(true);
     }
